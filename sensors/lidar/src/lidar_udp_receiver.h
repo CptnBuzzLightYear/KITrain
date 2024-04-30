0ef6f0c0d09a4ec3632a77063f5c9920ce545fd1
uetly <67153666+uetly@users.noreply.github.com>
2024-04-30 17:54:03 +0200

    Lirdar_Udp_socket added

---
+++ sensors/lidar/src/lidar_udp_receiver.h
@@ -0,0 +1,17 @@
+// lidar_udp_receiver.h
+#ifndef UDP_RECEIVER_H
+#define UDP_RECEIVER_H
+
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/header.hpp>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+
+
+
+#endif // UDP_RECEIVER_H
