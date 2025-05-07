e94f8e559ea0c676fa5869c810937048491e108d
CptnBuzzLightYear <s.schaefer@kit.edu>
2025-05-07 17:43:36 +0200

    Update sfy_dwarf_detector.py

--- src/nodes/driving_route_unlocker/signal_detector/signal_detector/sfy_dwarf_detector.py
+++ src/nodes/driving_route_unlocker/signal_detector/signal_detector/sfy_dwarf_detector.py
@@ -1,3 +1,5 @@
+// written by Furkan Yöndem 2024
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -86,70 +88,3 @@ if __name__ == '__main__':
     main()
 
 
-# import rclpy
-# from rclpy.node import Node
-# from sensor_msgs.msg import Image
-# from cv_bridge import CvBridge, CvBridgeError
-# import cv2
-# import numpy as np
-# from ultralytics import YOLO
-# import pandas as pd
-
-# # Load class names
-# with open('/home/steffens/KITrain/src/nodes/driving_route_unlocker/signal_detector/signal_detector/class_names.txt', "r") as my_file:
-#     data = my_file.read()
-# class_list = data.split("\n")
-# print(class_list)
-
-# class VideoSubscriber(Node):
-
-#     def __init__(self):
-#         super().__init__('video_subscriber')
-#         self.subscription = self.create_subscription(
-#             Image,
-#             '/received_image',
-#             self.video_callback,
-#             10)
-#         self.publisher_ = self.create_publisher(Image, 'dwarf_signal_detection', 10)
-#         self.bridge = CvBridge()
-#         self.model = YOLO("/home/steffens/KITrain/src/nodes/driving_route_unlocker/signal_detector/signal_detector/best.pt")  # Ensure 'best.pt' is in the src directory
-#         self.get_logger().info("Video subscriber node has been started.")
-
-#     def video_callback(self, ros_frame):
-#         try:
-#             frame = self.bridge.imgmsg_to_cv2(ros_frame, "bgr8")
-#             results = self.model.predict(frame)
-#             a = results[0].boxes.data.tolist()
-#             px = pd.DataFrame(a).astype("float")
-#             for index, row in px.iterrows():
-#                 x1 = int(row[0])
-#                 y1 = int(row[1])
-#                 x2 = int(row[2])
-#                 y2 = int(row[3])
-#                 d = int(row[5]) # klasse, d=0: sh1 (white), d=1 : hp0 (red)
-#                 c = class_list[d] # klassenbezeichnung
-#                 # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-#                 # cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0), 1)
-#                 if d == 0:
-#                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-#                     cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1)
-#                 else:
-#                     cv2.rectangle(frame, (x1, y1), (x2, y2), (242, 242, 242), 2)
-#                     cv2.putText(frame, str(c), (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, (139, 134, 130), 1)
-            
-#             # Convert the modified frame back to a ROS Image message and publish it
-#             ros_frame_out = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-#             self.publisher_.publish(ros_frame_out)
-        
-#         except CvBridgeError as e:
-#             self.get_logger().error(f"CvBridge Error: {e}")
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     video_subscriber = VideoSubscriber()
-#     rclpy.spin(video_subscriber)
-#     video_subscriber.destroy_node()
-#     rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
